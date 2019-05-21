52b5e71c4f596c61882d7aa008bbb2688331a217
Sandra Pekelis <spekelis@wireless-169-228-114-162.ucsd.edu>
2019-05-20 20:41:30 -0700

    Added title to MyGarden

--- lib/ui/myGarden.dart
+++ lib/ui/myGarden.dart
@@ -1,6 +1,7 @@
 import 'package:Growio/wdigets/myGardenTile.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
-import '../wdigets/plantTile.dart';
+
 
 class MyGarden extends StatefulWidget {
   @override
@@ -12,15 +13,43 @@ class _MyGardenState extends State<MyGarden> {
   @override
   Widget build(BuildContext context) {
 
+  /* Plantcyclopedia title */
+    Container _title(BuildContext context) {
+      return Container(
+        color: Colors.white,
+        alignment: Alignment.topCenter,
+        width: MediaQuery.of(context).size.width * (247/375),
+        child: AutoSizeText(
+          "MyGarden",
+          style: TextStyle(
+            color: Color(0xFF8BE4BB),
+            fontSize: 33,
+            fontFamily: 'Quicksand',
+            fontWeight: FontWeight.bold,
+          ),
+          maxLines: 1,
+        ),
+      );
+    }
+
     return Scaffold(
-      body: GridView.builder(
-        physics: BouncingScrollPhysics(),
-        itemCount: 8,
-        gridDelegate:
-          new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        itemBuilder: (BuildContext context, int position) {
-          return myGardenTile(context);
-        }, 
+      backgroundColor: Colors.white,
+      body: Column(
+        children: <Widget> [
+          _title(context),
+          Padding(padding: const EdgeInsets.all(10.0)),
+          Expanded(
+            child: GridView.builder(
+              physics: BouncingScrollPhysics(),
+              itemCount: 20,
+              gridDelegate:
+                new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+              itemBuilder: (BuildContext context, int position) {
+                return myGardenTile(context);
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
